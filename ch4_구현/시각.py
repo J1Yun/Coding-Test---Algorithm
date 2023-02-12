036ca47ff28c9e464f8059f977fa23cbe6b4a368
Choi-Ji-Yun <cjy5025@naver.com>
2023-02-12 15:04:40 +0900

    practice

---
+++ ch4_구현/시각.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 30 17:28:12 2021
+
+@author: Jiyun
+"""
+
+n = int(input())
+count = 0
+
+for i in range (n+1):
+    for j in range (60):
+        for k in range (60):
+            if '3' in str(i)+str(j)+str(k):
+                count += 1
+                
+print(count)                                                                                                                                                                                                                                                                                                      
