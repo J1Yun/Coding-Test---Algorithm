0a7443e0cb7183f2435aa913eea43adb5484ef2f
Choi-Ji-Yun <cjy5025@naver.com>
2023-05-02 09:10:07 +0900

    practice

---
+++ ch14_정렬문제/카드정렬하기.py
@@ -0,0 +1,15 @@
+import heapq
+
+n = int(input())
+data = []
+for _ in range(n):
+    heapq.heappush(data, int(input()))
+
+result = 0
+while len(data) > 1:
+    a = heapq.heappop(data)
+    b = heapq.heappop(data)
+    result += a + b
+    heapq.heappush(data, a+b)
+
+print(result)
