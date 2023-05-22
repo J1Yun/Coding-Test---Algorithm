a280259d6b50002cb6fc93caf45283f61a8fb66b
Choi-Ji-Yun <cjy5025@naver.com>
2023-05-22 10:40:32 +0900

    practice

--- ch8_다이나믹프로그래밍/효율적인화폐구성.py
+++ ch8_다이나믹프로그래밍/효율적인화폐구성.py
@@ -1,17 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 12 17:59:54 2021
-
-@author: Jiyun
-"""
-
 n, m = map(int, input().split())
 d = [10001] * (m+1)
 coin = []
 for i in range(n):
     coin.append(int(input()))
     
-d = [10001] * (m+1)
 d[0] = 0
 for i in coin:
     for j in range (i, m+1):
@@ -22,3 +14,24 @@ if d[m] == 10001:
     print(-1)
 else:
     print(d[m])
+
+
+
+# n, m = map(int, input().split())
+# d = [10001] * (m+1)
+# coin = []
+# for i in range(n):
+#     coin.append(int(input()))
+
+# d[0] = 0
+
+# for i in range(1, m+1):
+#     for c in coin:
+#         t = i - c
+#         if t >= 0 and d[t] != 10001:
+#             d[i] = min(d[i], d[t] + 1)
+
+# if d[m] == 10001:
+#     print(-1)
+# else:
+#     print(d[m])
